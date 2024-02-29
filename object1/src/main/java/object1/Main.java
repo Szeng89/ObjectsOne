5b2d0632c564235b675bbb8e5275828ee49976e5
Szeng89 <shijinz898@gmail.com>
2024-02-29 12:52:36 -0500

    took out the practice array, added dog array to main

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -1,10 +1,37 @@
 package object1;
 
-import java.util.logging.Logger;
+
 
 public class Main {
     public static void main(String... args) {
         Person person1 = new Person("Shijin", 34);
         System.out.println("height of person 3 : " + person1.height + " centimeters");
+
+
+
+       Dog[] dog = new Dog[4]; //how make array
+
+
+        Dog doggo1 = new Dog("Mocha", 11,"hhh");
+        Dog doggo2 = new Dog("Nala", 8, "Golden Poodle");
+        Dog doggo3 = new Dog("Cookie", 4,"Shiba Inu");
+        Dog doggo4 = new Dog("Nova", 5, "Labodor");
+
+         dog[0] = doggo4;
+         dog[1] = doggo3;
+         dog[2] = doggo2;
+         dog[3] = doggo1;
+
+
+        System.out.println(dog);
+
+
         }
+
+
+
+
+
+
+
 }
